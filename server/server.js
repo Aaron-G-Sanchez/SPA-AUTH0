7d1d1619f4e1ea28973cff92d22a82243b1399de
Aaron Sanchez <saarong12@gmail.com>
2024-02-18 01:54:27 -0600

    Added authorization via access token to server and set up trial route.
    Set up .env variables and set up first route. Tested with removing app.use(jwtCheck) and works as intended.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
-const app = require('./src/app')
-const { db } = require('./db/connection')
+const { app } = require('./src/app')
 const port = 3001
 
 app.listen(port, () => {
-  db.sync()
   console.log(`Listening at http://localhost:${port}/`)
 })
---
+++ server/src/app.js
@@ -0,0 +1,25 @@
+require('dotenv').config('.env')
+const express = require('express')
+const app = express()
+
+const { auth } = require('express-oauth2-jwt-bearer')
+
+const { AUTH0_SECRET, AUTH0_AUDIENCE, AUTH0_BASE_URL, AUTH_SIGNING_ALGO } =
+  process.env
+
+const jwtCheck = auth({
+  secret: AUTH0_SECRET,
+  audience: AUTH0_AUDIENCE,
+  issuerBaseURL: AUTH0_BASE_URL,
+  tokenSigningAlg: AUTH_SIGNING_ALGO
+})
+
+app.use(jwtCheck)
+
+app.get('/users', (req, res) => {
+  res.send('ACCESS GRANTED')
+})
+
+module.exports = {
+  app
+}
